d3f9032948c47d66032964d9080d952fbea1477d
Yu-Cheng Wang <brian0978599918@gmail.com>
2022-12-26 22:33:10 +0800

    The software building vol.1.

--- clipQ_fruit_detection/parser_argu.py
+++ clipQ_fruit_detection/parser_argu.py
@@ -1,25 +1,32 @@
-def init_parse(parser) :
-    # modify the argument here
+def init_parse(parser) :        # modify the argument here
+    # The core type to run model
     parser.add_argument('--cpu', action='store_true',
                         help='set if only CPU is available')
-    parser.add_argument('--data', action='store', default='./data/',
-                        help='dataset path')
+    # Choose the Model structure
     parser.add_argument('--arch', action='store', default='nin',
                         help='the architecture for the network: nin')
+    # Choose the type of optimizer
     parser.add_argument('--opt', action='store', default='Adam',
                         help='the optimizer')
+    # learning rate for optimizer
     parser.add_argument('--lr', action='store', default='0.1',
                         help='the intial learning rate')
+    # load trained model's dict
     parser.add_argument('--pretrained', action='store', default=None,
                         help='the path to the pretrained model')
-    parser.add_argument('--evaluate', action='store_true',
-                        help='evaluate the model')
+    # full precision for Conv2d
     parser.add_argument('--full', action='store', default='0',
                         help='full precision or not')
+    # Total epoch the model train & validate
     parser.add_argument('--epoch', action='store', default='300',
                         help='training epoch')
+    # write the model's parameter into .HEX file
     parser.add_argument('--write', action='store_true',
                         help='write_files')
+    # Validating model
+    parser.add_argument('--evaluate', action='store_true',
+                        help='evaluate the model')
+    # Testing the data
     parser.add_argument('--predict', action='store_true',
                         help='predict single picture')
     return parser
