10c438826c41f1d9e28312fe597b33d91a1498c2
Yu-Cheng Wang <brian0978599918@gmail.com>
2022-12-29 18:13:02 +0800

    The software finished at 12/29

--- clipQ_fruit_detection/model/test.py
+++ clipQ_fruit_detection/model/test.py
@@ -1,12 +1,10 @@
 from torch.autograd import Variable
+import random
 
 def test(full, model, bin_op, testloader):
     model.eval()
     if full == 0:
         bin_op.NbitClipQ()
-
-    withMask_cnt = 0
-    withoutMask_cnt = 0
 
     for data, target in testloader:
         data, target = Variable(data.cuda()), Variable(target.cuda())
@@ -14,15 +12,13 @@ def test(full, model, bin_op, testloader):
         output = model(data)
         pred = output.data.max(1, keepdim=True)[1]
 
-        # print(pred)
-        if(pred[0] == 0):
-            print("[Result] With mask!")
-            withMask_cnt += 1
+        # Choose one picture in the testloader
+        index = int(random.uniform(0, target.size(dim=0)))
+        if(target[index] == pred[index]):
+            print("The chosen data {} is perdict correctly".format(index))
+            break
         else:
-            print("[Result] Without mask")
-            withoutMask_cnt += 1
-
-        print('WithMask: ', withMask_cnt,', withoutMask_cnt: ', withoutMask_cnt)
+            print("The chosen data {} is perdict wrong".format(index))
 
     if full == 0:
         bin_op.restore()
